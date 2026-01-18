97e0dc47a9b870243007578a62a04d13ebe4275d
Caleb <caleb.hunt789@gmail.com>
2026-01-18 02:19:55 +0000

    almost done the first step

---
+++ assignments/labs/lab_one/car.php
@@ -0,0 +1,16 @@
+<?php
+// footer.php
+// Shared footer content. Included from index.php.
+// the start of the journey
+
+//car class
+
+class car{
+  public string $make;
+  public string $model;
+  public int $year;
+
+  public function __construct( string $make, string $model,int $year){}
+}
+?>
+  
